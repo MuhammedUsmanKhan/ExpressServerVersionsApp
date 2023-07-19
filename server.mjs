f5f3e886da8f45fa1634884c2aca85e22c8329e5
MuhammedUsmanKhan <uktech0310@gmail.com>
2023-07-19 11:18:54 +0500

    ExpressServerDoneJustGuidlinesAreLeft

---
+++ server.mjs
@@ -0,0 +1,18 @@
+import express from 'express'
+const app = express()
+const port = 3000
+import path from 'path';
+const __dirname = path.resolve();
+//app.use(express.json());
+
+import V1Route from './versions/ApiV1/apiv1.mjs'
+import V2Route from './versions/ApiV2/apiv2.mjs'
+
+app.use('/api/v1',V1Route)
+app.use('/api/v2',V2Route)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
